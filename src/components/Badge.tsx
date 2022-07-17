04adf2636d18f0bbb77baf1a47840776ee3f4a60
Alex Kurov <Alex>
2022-07-17 20:51:34 +0300

    feat(country-list): complete the project

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Color } from "../types";
 
-export const Badge = () => {
+interface IBadge {
+  color: Color;
+  text: string
+}
+
+export const Badge = ({color, text} : IBadge) => {
   return (
-    <div>Badge</div>
+    <button className={`badge bg-${color} col-2 m-0`}>{text}</button>
   )
 }
