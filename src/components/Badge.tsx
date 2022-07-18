b34ff0d93cc0569a1cee9d71a341813cf0d9347e
Alex Kurov <twistter8@gmail.com>
2022-07-18 12:23:52 +0300

    refactor(country-list): chenged interface IBadge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,10 +1,11 @@
-import { Color } from "../types";
+import { BadgeLabel, Color } from "../types";
 
 interface IBadge {
+  label: BadgeLabel,
   color: Color;
-  text: string;
+  value: number;
 }
 
-export const Badge = ({ color, text }: IBadge) => {
-  return <button className={`badge bg-${color} col-2 m-0`}>{text}</button>;
+export const Badge = ({ color, value, label }: IBadge) => {
+  return <button className={`badge bg-${color} col-2 m-0`} >{`${label}: ${value}`}</button>;
 };
